be9d2f52f863df685e5daed9d337ad7252665217
Jorge Loredo <jorgel@teamventi.com>
2023-09-27 17:02:54 -0600

    Contenido de video 1 y 2 agregado

---
+++ SistemaVenta.IOC/Dependencia.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SistemaVenta.DAL.DBContext;
+using SistemaVenta.DAL.Implementacion;
+using SistemaVenta.DAL.Interfaces;
+using SistemaVenta.BLL.Implementacion;
+using SistemaVenta.BLL.Interfaces;
+
+namespace SistemaVenta.IOC
+{
+    public static class Dependencia
+    {
+        public static void InyectarDependencia(this IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddDbContext<DbventaContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("CadenaSQL"));
+            });
+        }
+    }
+}
